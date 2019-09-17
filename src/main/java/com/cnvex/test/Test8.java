2c54726f35f8216bf63ad06796280eb2da5c44f3
dhr <117373604@qq.com>
2019-09-17 17:57:22 +0800

    ignore plugin

---
+++ src/main/java/com/cnvex/test/Test8.java
@@ -0,0 +1,12 @@
+package com.cnvex.test;
+
+public class Test8 {
+	
+	public static void main(String[] args) {
+		
+		// 111
+		// 222
+		
+	}
+
+}
---
+++ src/main/java/com/cnvex/test/Test9.java
@@ -0,0 +1,12 @@
+package com.cnvex.test;
+
+public class Test9 {
+	
+	public static void main(String[] args) {
+		
+		// 111
+		// 222
+		
+	}
+
+}
